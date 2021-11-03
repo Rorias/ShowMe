bd0e1f6284b7dc36118eccf448f5cf4487051935
Lars Mulder <larsmulderrr@gmail.com>
2021-11-03 15:51:05 +0100

    now many objects can be activated

--- Assets/Scripts/Player/ActivateObject.cs
+++ Assets/Scripts/Player/ActivateObject.cs
@@ -6,14 +6,17 @@ using UnityEngine.Events;
 
 public class ActivateObject : MonoBehaviour
 {
-    public GameObject objectToActivate;
+    public List<GameObject> objectsToActivate;
     public UnityEvent action = new UnityEvent();
 
     private void OnCollisionEnter2D(Collision2D _coll)
     {
         if (_coll.gameObject.tag == "Player")
         {
-            objectToActivate.SetActive(true);
+            for (int i = 0; i < objectsToActivate.Count; i++)
+            {
+                objectsToActivate[i].SetActive(true);
+            }
             action.Invoke();
             Destroy(gameObject);
         }
